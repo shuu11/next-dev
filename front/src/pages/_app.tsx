069ddd1a6ac2ff74e4f5de37077187ac6b5168d2
shuu11 <shuu1104main@gmail.com>
2022-12-30 17:11:28 +0900

    change

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,56 +1,89 @@
-import Checkbox from '@mui/material/Checkbox'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { createGlobalStyle } from 'styled-components'
-import useSWR from 'swr'
-import ContentLoader from 'react-content-loader'
-import HomeIcon from '@mui/icons-material/Home'
-import CheckBox from 'components/molecules/CheckBox'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { SWRConfig } from 'swr'
+import GlobalSpinner from 'components/organisms/GlobalSpinner'
+import { AuthContextProvider } from 'contexts/AuthContext'
+import GlobalSpinnerContextProvider from 'contexts/GlobalSpinnerContext'
+import { ShoppingCartContextProvider } from 'contexts/ShoppingCartContext'
+import { theme } from 'themes'
+import type { ApiContext } from 'types'
+import { fetcher } from 'utils'
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  textarea{
-		padding:0;
-		margin:0;
-		font-family:-apple-system,-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
+html,
+body,
+textarea {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
 
-	* {
-		box-sizing:border-box;
-	}
+* {
+  box-sizing: border-box;
+}
 
-	a {
-		cursor:pointer;
-		text-decoration:none;
-		transition:0.25s;
-		color:#000;
-	}
+a {
+  cursor: pointer;
+  text-decoration: none;
+  transition: .25s;
+  color: ${theme.colors.black};
+}
 
-	ol,ul{
-		list-style:none;
-	}
+ol, ul {
+  list-style: none;
+}
 `
 
-
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<meta key="charset" name="charset" content="utf-8" />
-				<meta
-					key="viewport"
-					name="viewport"
-					content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-size=5"
-				/>
-				<meta property="og:locale" content="ja_JP" />
-				<meta property="og:type" content="website" />
-			</Head>
-			<GlobalStyle />
-
-			<Component {...pageProps} />
-		</>
-	)
+const context: ApiContext = {
+  apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
 }
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        {/* <title key="title">{SITE_TITLE}</title>
+        <meta name="title" content={SITE_TITLE} key="meta:title" />
+        <meta name="description" content={SITE_DESCRIPTION} key="meta:description" />
+        <meta property="og:title" content={SITE_TITLE} key="meta:og:title" />
+        <meta property="og:description" content={SITE_DESCRIPTION} key="meta:og:description" />
+        <meta property="og:image" content={`${publicRuntimeConfig.domainUrl}/static/images/icon/icon-512.png`} key="meta:og:image" />
+        <meta property="og:site_name" content={SITE_NAME} /> */}
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
+        {/* <meta property="fb:app_id" content="556485011968079" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@truck2hand" /> */}
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <SWRConfig
+          value={{
+            shouldRetryOnError: false,
+            fetcher,
+          }}
+        >
+          <GlobalSpinnerContextProvider>
+            <ShoppingCartContextProvider>
+              <AuthContextProvider context={context}>
+                <GlobalSpinner />
+                <Component {...pageProps} />
+              </AuthContextProvider>
+            </ShoppingCartContextProvider>
+          </GlobalSpinnerContextProvider>
+        </SWRConfig>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
